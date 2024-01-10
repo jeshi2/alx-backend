b011aef7c5238cd3b39e76d2606d5730a2ea369f
jeshi2 <jamesvean19@gmail.com>
2024-01-10 15:17:25 +0300

    wow

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -11,13 +11,3 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
-
-
-if __name__ == "__main__":
-    res = index_range(1, 7)
-    print(type(res))
-    print(res)
-
-    res = index_range(page=3, page_size=15)
-    print(type(res))
-    print(res)
