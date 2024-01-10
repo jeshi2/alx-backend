916a0d86037ef9b14cb11732cc6e5815f0e94bc5
jeshi2 <jamesvean19@gmail.com>
2024-01-10 18:45:59 +0300

    page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 
-import csv
-from typing import List
-from math import ceil
-
 """
 simple pagination
 """
 
-
-def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """
-    Return a tuple of start and end indices for a given page and page_size.
-
-    Returns:
-        tuple: A tuple containing the start and end indices
-        for the specified page.
-    """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return start_index, end_index
+import csv
+from typing import List, Tuple
 
 
 class Server:
@@ -42,23 +28,21 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Finds the correct indexes to paginate dataset.
         """
-        Return the specified page of the dataset based on
-        pagination parameters.
-
-        Returns:
-            List[List]: A list of rows representing the
-            specified page of the dataset.
-        """
-        assert isinstance(
-            page, int) and page > 0, "Page be a positive integer"
-        assert isinstance(
-            page_size, int) and page_size > 0, "Page be a positive integer"
-
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+        csv_size = len(self.dataset())
         start, end = index_range(page, page_size)
-        dataset = self.dataset()
-
-        if start >= len(dataset):
+        end = min(end, csv_size)
+        if start >= csv_size:
             return []
+        return self.dataset()[start:end]
 
-        return dataset[start:end]
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Returns a tuple containing a start and end index.
+    """
+    return ((page - 1) * page_size, page * page_size)
