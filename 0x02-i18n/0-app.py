0272cbcf615ff7acfe240343aa42817f7dda45fe
jeshi2 <jamesvean19@gmail.com>
2024-01-13 20:49:36 +0300

    app route

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with a single / route.
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template(
+        '0-index.html',
+        title='Welcome to Holberton',
+        header='Hello world')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
